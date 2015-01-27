11c951985e1ad932c8b7dfab64a8817344c517fb
Jonathan Chen <ypeels@live.com>
2015-01-27 10:21:04 -0600

    unused '~' variable is Octave-incompatible

--- ex1/multi_classifier_accuracy.m
+++ ex1/multi_classifier_accuracy.m
@@ -1,5 +1,5 @@
 function accuracy=binary_classifier_accuracy(theta, X,y)
-  [~,labels] = max(theta'*X, [], 1);
+  [unused_,labels] = max(theta'*X, [], 1);
 
   correct=sum(y == labels);
   accuracy = correct / length(y);
