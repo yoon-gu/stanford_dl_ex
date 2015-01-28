fba46d5a870eaf780ab0ee177390f19101791125
Jonathan Chen <ypeels@live.com>
2015-01-28 15:41:13 -0600

    duh, gradStack initialization should be just like stack's

--- multilayer_supervised/supervised_dnn_cost.m
+++ multilayer_supervised/supervised_dnn_cost.m
@@ -11,36 +11,36 @@ if exist('pred_only','var') % uh, does matlab automatically do optional argument
   po = pred_only;
 end;
 cost = 0;
-grad = zeros(size(theta)); % should work, except gradStack code will kill this
+grad = zeros(size(theta));
 pred_prob = 0;
 
-%%% reshape into network
-%stack = params2stack(theta, ei);
-%numHidden = numel(ei.layer_sizes) - 1;
-%hAct = cell(numHidden+1, 1);
-%gradStack = cell(numHidden+1, 1);
-%
-%%% forward prop
-%%%% YOUR CODE HERE %%%
-%
-%%% return here if only predictions desired.
-%if po
-%  %cost = -1; %ceCost = -1; wCost = -1; numCorrect = -1;
-%  %grad = [];  
-%  return;
-%end;
-%
-%%% compute cost
-%%%% YOUR CODE HERE %%%
-%
-%%% compute gradients using backpropagation
-%%%% YOUR CODE HERE %%%
-%
-%%% compute weight penalty cost and gradient for non-bias terms
-%%%% YOUR CODE HERE %%%
-%
-%%% reshape gradients into vector
-%[grad] = stack2params(gradStack);
+%% reshape into network
+stack = params2stack(theta, ei);
+numHidden = numel(ei.layer_sizes) - 1;
+hAct = cell(numHidden+1, 1);
+gradStack = params2stack(grad, ei);
+
+%% forward prop
+%%% YOUR CODE HERE %%%
+
+%% return here if only predictions desired.
+if po
+  %cost = -1; %ceCost = -1; wCost = -1; numCorrect = -1;
+  %grad = [];  
+  return;
+end;
+
+%% compute cost
+%%% YOUR CODE HERE %%%
+
+%% compute gradients using backpropagation
+%%% YOUR CODE HERE %%%
+
+%% compute weight penalty cost and gradient for non-bias terms
+%%% YOUR CODE HERE %%%
+
+%% reshape gradients into vector
+[grad] = stack2params(gradStack);
 end
 
 
