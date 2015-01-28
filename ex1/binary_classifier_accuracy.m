d5b42a60c5ed0b74a5f3f5d3651c876b7f1ac473
Jonathan Chen <ypeels@live.com>
2015-01-27 20:48:57 -0600

    oops, corrected the wrong file

--- ex1/binary_classifier_accuracy.m
+++ ex1/binary_classifier_accuracy.m
@@ -1,3 +1,3 @@
-function binary_classifier_accuracy=binary_classifier_accuracy(theta, X,y)
+function accuracy=binary_classifier_accuracy(theta, X,y)
   correct=sum(y == (sigmoid(theta'*X) > 0.5));
-  binary_classifier_accuracy = correct / length(y);
+  accuracy = correct / length(y);
