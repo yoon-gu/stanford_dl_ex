e79c7a844e1678c9098ef48a1187d98f4b67524f
Jonathan Chen <ypeels@live.com>
2015-01-31 11:25:34 -0600

    Exercise - Convolution and Pooling. easy!

--- cnn/cnnPool.m
+++ cnn/cnnPool.m
@@ -29,7 +29,33 @@ pooledFeatures = zeros(convolvedDim / poolDim, ...
 %   
 %   Use mean pooling here.
 
-%%% YOUR CODE HERE %%%
+    % """
+    % This can be done
+    % efficiently using the conv2 function as well. The inputs are the responses of each    
+    % image with each filter computed in the previous step. Convolve each of these with a
+    % matrix of ones followed by a subsampling and averaging. Make sure to use the “valid”
+    % border handling convolution.
+    % """
+    % this method sounds wasteful...doesn't subsampling throw away a bunch of work?
+    % oh but the alternative is a loop over pooling coordinates, which is even slower??
+        % but wait, if you can do the subsampling AFTER, couldn't you do it before too??
+        % i guess it's a single line to subsample, but a loop to get blocks?
+    %assert(mod(convolvedDim, poolDim) == 0); % meh, implicit in pooledFeatures allocation above
+    for imageNum = 1:numImages
+        for filterNum = 1:numFilters        
+            features = convolvedFeatures(:,:,filterNum,imageNum);
+            convolved = conv2(features, ones(poolDim, poolDim), 'valid');
+            subsampled = convolved(1:poolDim:convolvedDim, 1:poolDim:convolvedDim);            
+            pooledFeatures(:,:, filterNum, imageNum) = subsampled / (poolDim^2);
+        end
+    end
+    
+    % alternative approach : meh
+        % get vector of blocks
+        % apply mean(mean()) to each block
+        % reshape to matrix
+    
+    
 
 end
 
