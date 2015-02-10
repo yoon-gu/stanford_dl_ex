4921dde4c888ae0b7747790e3a6c4f2f5fe97577
Jonathan Chen <ypeels@live.com>
2015-02-10 16:45:15 -0600

    Oh I think I finally found my bug. RICA example currently fails gradient check.

--- rica/softICACost.m
+++ rica/softICACost.m
@@ -7,9 +7,9 @@ Wgrad = zeros(size(W));
 
 % project weights to norm ball (prevents degenerate bases)
 Wold = W;
-W = l2rowscaled(W, 1);
+W = l2rowscaled(W, 1); % norm constraint? cf. lecture notes p. 17 - but isn't that for visualization?
     % the 2 lines above are ignorable preprocessing that gets popped before returning?
-
+   
     % backprop was done ANALYTICALLY in tutorial    
     z2 = W*x;
     reconstructionError = W'*z2 - x;
@@ -25,6 +25,7 @@ W = l2rowscaled(W, 1);
     Wgrad = params.lambda * z2*bsxfun(@rdivide, x, softPenalties)' + ... % this matrix derivative checks numerically
         W*(reconstructionError)*x' + (z2)*(reconstructionError)'; % really, just copy their one-liner / 2??
 
+%     grad = Wgrad;    
 % unproject gradient for minFunc
 grad = l2rowscaledg(Wold, W, Wgrad, 1);
 grad = grad(:);
@@ -35,6 +36,9 @@ end
 
 function L1 = l1norm(v)
     % "In this exercise, we find epsilon = 0.01 to work well."
+    % ohhhh i have the wrong formula for L1 norm. that means the gradient is wrong too.
+        % so i had the right gradient for the WRONG FORMULA. that'll do 'er.
     global params;
-    L1 = sqrt(sum(v.^2, 1) + params.epsilon);
+    %L1 = sqrt(sum(v.^2, 1) + params.epsilon);
+    L1 = sum(sqrt(v.^2 + params.epsilon), 1);
 end
