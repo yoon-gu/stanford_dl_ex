e1ccce7815c8aa9ac3693cb38ceb466484fc1be4
Jonathan Chen <ypeels@live.com>
2015-02-10 21:59:48 -0600

    gradient vectorized! much easier to start from verified unvectorized implementation than to bang your head against pencil/paper

--- rica/runSoftICA.m
+++ rica/runSoftICA.m
@@ -6,21 +6,42 @@ addpath(genpath('../common/')) % path to minfunc
 %% These parameters should give you sane results. We recommend experimenting
 % with these values after you have a working solution.
 global params;
-DEBUG = false;
+DEBUG = true;
+TEST_STL = false;
 if DEBUG
     params.m = 100;
     params.patchWidth = 3;
-else
-    params.m=10000;% num patches
+    params.numFeatures = 5;
+else    
+    params.m=10000;%200000;% num patches
     params.patchWidth=9;% width of a patch
+    params.numFeatures = 50; %50 % number of filter banks to learn
 end
 params.n=params.patchWidth^2; % dimensionality of input to RICA
-params.lambda = 0.0005; % sparsity cost
-params.numFeatures = 50; % number of filter banks to learn
-params.epsilon = 1e-2; % epsilon to use in square-sqrt nonlinearity
+params.lambda = 0.0005;%;%0.0005; % sparsity cost
+params.epsilon = 1e-2;% epsilon to use in square-sqrt nonlinearity
+zcaEpsilon = 1e-4; %%% originally 1e-4
 
 % Load MNIST data set
 data = loadMNISTImages('../common/train-images-idx3-ubyte');
+
+% emulate parameters for STL exercise
+if TEST_STL
+    params.m = 200000;
+    params.numFeatures = 32;
+    data = sparse(data); % for 32-bit MATLAB - eat the overhead in Octave
+    data = data(:, 1:round(size(data, 2) * (5/6)));
+    data = full(data);
+end
+
+% random selection of patches for visualization
+if isOctave(); 
+    call_randi = @(imax, sz1, sz2) 1 + round((imax-1)*rand(sz1, sz2)); 
+else
+    rng('default'); % get numbers to match (most) of the figures
+    call_randi = @randi; % otherwise it becomes undefined...stupid MATLAB...
+end
+randsel = call_randi(params.m,200,1); % A random selection of samples for visualization
 
 %% Preprocessing
 % Our strategy is as follows:
@@ -30,12 +51,29 @@ data = loadMNISTImages('../common/train-images-idx3-ubyte');
 
 % Step 1) Sample patches
 patches = samplePatches(data,params.patchWidth,params.m);
+figure('name', 'Raw patches'); display_network(patches(:, randsel));
+if ~isOctave(); patches = single(patches); end
+
 % Step 2) Apply ZCA
-patches = zca2(patches);
+%for p=-6:3
+%    x = zca2(patches, 10^p);
+%    m = sqrt(sum(patches.^2) + (1e-8));
+%    x = bsxfunwrap(@rdivide,patches,m);
+%    figure('name', sprintf('ZCA-whitened with eps = 10^%d', p));
+%    display_network(x(:, randsel));
+%end
+%return
+
+patches = zca2(patches, zcaEpsilon);
+if ~DEBUG; figure('name', 'ZCA-whitened patches'); display_network(patches(:, randsel)); end
+
 % Step 3) Normalize each patch. Each patch should be normalized as     % PCA section: variance normalization is NOT needed??
 % x / ||x||_2 where x is the vector representation of the patch 
+% x = patches;
 m = sqrt(sum(patches.^2) + (1e-8));
 x = bsxfunwrap(@rdivide,patches,m);
+
+
 
 
 %% Run the optimization
@@ -43,7 +81,9 @@ options.Method = 'lbfgs';
 options.MaxFunEvals = Inf;
 options.MaxIter = 500;
 %options.display = 'off';
-options.outputFcn = @showBases;
+% options.outputFcn = @showBases;
+if ~isOctave; options.useMex = false; end
+
 
 % initialize with random weights
 randTheta = randn(params.numFeatures,params.n)*0.01; % 1/sqrt(params.n);
@@ -64,10 +104,15 @@ if DEBUG
     assert(diff < 1e-6,...
         'Difference too large. Check your gradient computation again');    
 end
+fprintf('derivatives checked!\n')
+return;
 
 % optimize
+tic;
 [opttheta, cost, exitflag] = minFunc( @(theta) softICACost(theta, x, params), randTheta, options); % Use x or xw
+fprintf('Optimization time (sec): %g\n', toc);
 
 % display result
 W = reshape(opttheta, params.numFeatures, params.n);
+figure('name', 'Final filters')
 display_network(W');
